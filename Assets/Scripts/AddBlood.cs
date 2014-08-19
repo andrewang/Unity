688cfb7650ffb61c7687c2d02ab4e5b3de575200
vitah <linw1225@gmail.com>
2014-08-19 16:31:24 +0800

    .

---
+++ Assets/Scripts/AddBlood.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2014 Blueant Inc. All Rights Reserved.
+
+/**
+ * @created 2014/08/19
+ * @filename AddBlood.cs
+ * @author linw1225@163.com(vitah)
+ * @fileoverview 场景scene1脚本，血量增加
+ */
+using UnityEngine;
+using System.Collections;
+
+public class AddBlood : MonoBehaviour {
+
+    public UISlider _blood_slider;
+
+	void OnClick()
+    {
+        _blood_slider.value += 0.1f;
+    }
+}
---
+++ Assets/Scripts/CutBlood.cs
@@ -0,0 +1,21 @@
+﻿// Copyright 2014 Blueant Inc. All Rights Reserved.
+
+/**
+ * @created 2014/08/19
+ * @filename AddBlood.cs
+ * @author linw1225@163.com(vitah)
+ * @fileoverview 场景scene1脚本，血量减少
+ */
+using UnityEngine;
+using System.Collections;
+
+public class CutBlood : MonoBehaviour {
+
+    // 表示血条的进度条
+    public UISlider _blood_slider;
+
+    void OnClick()
+    {
+        _blood_slider.value -= 0.1f; 
+    }
+}
